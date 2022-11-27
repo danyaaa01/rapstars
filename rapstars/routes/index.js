c94785a8d863ff48ff1341fb95c73053eb2f832f
danyaaa01 <danya.bibik16@mail.ru>
2022-11-27 20:00:55 +0300

    Были изменены страницы артистов, а именно добавлена биография и фотографии.

--- rapstars/routes/index.js
+++ rapstars/routes/index.js
@@ -8,17 +8,29 @@ router.get('/', function(req, res, next) {
 
 /* Страница Gunna */
 router.get('/gunna', function(req, res, next) {
-  res.send("<h1>Страница Gunna</h1>")
+  res.render('rappers', {
+    title: "Gunna",
+    picture: "images/gunna.png",
+    desc: "Gunna — американский рэпер. Наиболее известен благодаря его совместным композициям с Янг Тагом и Lil Baby, он подписан на лейбл Тага — YSL Records, а также 300 Entertainment и Atlantic Records. Его дебютный студийный альбом Drip or Drown 2 вышел 22 февраля 2019."
+  })
 });
 
 /* Страница Drake */
 router.get('/drake', function(req, res, next) {
-  res.send("<h1>Страница Drake</h1>")
+  res.render('rappers', {
+    title: "Drake",
+    picture: "images/drake.jpg",
+    desc: "О́бри Дрейк Грэм — канадский рэпер, певец, автор песен, музыкальный продюсер, актёр и предприниматель. Получил первое признание в качестве актёра в телевизионном подростковом сериале «Деграсси: Следующее поколение»."
+  });
 });
 
-/* Страница Коржика */
+/* Страница Young Thug */
 router.get('/young_thug', function(req, res, next) {
-  res.send("<h1>Страница Young Thug</h1>")
+  res.render('rappers', {
+    title: "Young Thug",
+    picture: "images/young_thug.jpg",
+    desc: "Дже́ффери Лама́р Уи́льямс, выступающий под сценическим именем Янг Таг — американский рэпер, певец и автор песен. Известный своим эксцентричным стилем вокала и отношением к моде, первое внимание к себе он привлёк совместными работами с такими рэперами, как Рич Хоуми Куан, Бердмен и Гуччи Мейн."
+  });
 });
 
 module.exports = router;
