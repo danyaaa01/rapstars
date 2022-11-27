9a08fcc23a06644dec3e8a1c219f68ee3600fdb0
danyaaa01 <danya.bibik16@mail.ru>
2022-11-27 14:54:30 +0300

    Были добавлены 3 новых страницы для разных исполнителей: Gunna, Drake, Young Thug.

--- rapstars/routes/index.js
+++ rapstars/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Gunna */
+router.get('/gunna', function(req, res, next) {
+  res.send("<h1>Страница Gunna</h1>")
+});
+
+/* Страница Drake */
+router.get('/drake', function(req, res, next) {
+  res.send("<h1>Страница Drake</h1>")
+});
+
+/* Страница Коржика */
+router.get('/young_thug', function(req, res, next) {
+  res.send("<h1>Страница Young Thug</h1>")
+});
+
 module.exports = router;
