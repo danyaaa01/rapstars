ae61cd3d7518ffe0b0db1494c67c70cd1fe46012
danyaaa01 <danya.bibik16@mail.ru>
2022-12-02 22:49:05 +0300

    Был проделан посев данных приложения

--- rapstars/createDB.js
+++ rapstars/createDB.js
@@ -1,6 +1,5 @@
 var MongoClient = require('mongodb').MongoClient
 var data = require("./data.js").data
-
 const uri = "mongodb://localhost:27017/"
 const client = new MongoClient(uri)
 async function run() {
@@ -10,8 +9,8 @@ var database = client.db("rapstars");
 database.dropDatabase()
 database = client.db("rapstars");
 const cats = database.collection("rappers");
-const result = await cats.insertOne({name:"Юра Авангард"});
-console.log(`${result} documents were inserted`);
+const result = await cats.insertMany(data);
+console.log(`${result.insertedCount} documents were inserted`);
 } finally {
 await client.close();
 }
